b2e6d19227294649474e8bbe320ce40a869c084f
Dakshan Kumar A <scholar.dakshanrs@gmail.com>
2025-04-16 18:09:13 +0000

    Backend started

---
+++ api/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.json("Hello World");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running at Port ${PORT}`);
+});
