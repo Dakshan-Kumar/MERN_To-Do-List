71e9281f0ab6c1003f19df7343014de589f30955
Dakshan Kumar A <scholar.dakshanrs@gmail.com>
2025-04-18 11:27:33 +0000

    added Routes in backend

--- api/views/toDoRoutes.js
+++ api/views/toDoRoutes.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const todoController = require("../controllers/toDoController");
+const { RouterSharp } = require("@mui/icons-material");
 
-router.get("/")
+router.get("/", todoController.getAllTodos);
+
+router.post("/", todoController.createTodo);
+
+router.put("/:id", todoController.updateTodo);
+
+router.delete("/:id", todoController.deleteTodo);
+
+module.exports = router;
